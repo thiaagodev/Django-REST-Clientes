209275c6c1b94d5283c26af9bf9afb500c4e1b7c
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-12 18:45:31 -0300

    Validando dados no serializer

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -6,3 +6,22 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
     
+    def validate_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError('O CPF deve ter 11 dígitos!')
+        return cpf
+
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError('Não inclua números neste campo!')
+        return nome
+    
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError('O RG deve ter 9 dígitos!')
+        return rg
+
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError('O celular deve ter no mínimo 11 dígitos!')
+        return celular
