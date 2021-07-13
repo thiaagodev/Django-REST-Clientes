37672cdda1425661d2abfee3cac75e2a213d09a3
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-13 17:54:42 -0300

    Paginação de clientes

--- clientes/admin.py
+++ clientes/admin.py
@@ -7,7 +7,7 @@ class Clientes(admin.ModelAdmin):
     search_fields = ('nome',)
     list_filter = ('ativo',)
     list_editable = ('ativo',)
-    list_per_page = 25
+    list_per_page = 10
 
 admin.site.register(Cliente, Clientes)
 
