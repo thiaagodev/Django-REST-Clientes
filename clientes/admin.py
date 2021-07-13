86ec986dc47f64ac50da55095ecfc16afbbb9f98
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-13 18:00:39 -0300

    Ordenação de nomes com django_filters

--- clientes/admin.py
+++ clientes/admin.py
@@ -8,6 +8,7 @@ class Clientes(admin.ModelAdmin):
     list_filter = ('ativo',)
     list_editable = ('ativo',)
     list_per_page = 10
+    ordering = ('nome',)
 
 admin.site.register(Cliente, Clientes)
 
--- setup/settings.py
+++ setup/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'clientes'
+    'django_filters',
+    'clientes',
 ]
 
 MIDDLEWARE = [
